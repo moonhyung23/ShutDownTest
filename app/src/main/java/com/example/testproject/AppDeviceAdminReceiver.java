42562f9a3f8ead571e69cd1a28d5d5944d920b7b
DESKTOP-Q9M8J51\이문형 <moonhyung.lee@wewakecorp.com>
2023-10-19 15:44:14 +0900

    dsadasdasd

---
+++ app/src/main/java/com/example/testproject/AppDeviceAdminReceiver.java
@@ -0,0 +1,23 @@
+package com.example.testproject;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class AppDeviceAdminReceiver extends DeviceAdminReceiver {
+    private static final String TAG = AppDeviceAdminReceiver.class.getSimpleName();
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+        Log.d(TAG, "Admin onEnabled");
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        super.onDisabled(context, intent);
+        Log.d(TAG, "Admin onDisabled");
+    }
+}
---
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.testproject;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private DevicePolicyManager devicePolicyManager;
+    Button button;
+    private int DEVICE_ADMIN_ADD_RESULT_ENABLE = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnTest);
+        devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adminActive();
+            }
+        });
+    }
+
+    private void adminActive() {
+        ComponentName componentName = new ComponentName(this, AppDeviceAdminReceiver.class);
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+        startActivityForResult(intent, DEVICE_ADMIN_ADD_RESULT_ENABLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == DEVICE_ADMIN_ADD_RESULT_ENABLE) {
+//            setCameraDisabled(true);
+        }
+
+        if (requestCode == 10 && resultCode == RESULT_OK) {
+            // 앱이 기기 소유자로 설정됨
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
